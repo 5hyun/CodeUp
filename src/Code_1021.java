8c32f0804e359fcf8bf2ade2f24b589f8e0eeb88
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-15 22:35:26 +0900

    코드업 1021

---
+++ src/Code_1021.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Code_1021 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String x = scanner.next();
+		System.out.println(x);
+	}
+}
