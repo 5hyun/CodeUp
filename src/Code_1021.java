4170d4ec6ac5291bcbbf965d8c1ca9332fe61573
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-15 22:39:10 +0900

    코드업 1022

--- src/Code_1021.java
+++ src/Code_1021.java
@@ -4,7 +4,7 @@ public class Code_1021 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		String x = scanner.next();
+		String x = scanner.nextLine();//중간에 공백이 있을시 모두 출력해주는 함수
 		System.out.println(x);
 	}
 }
