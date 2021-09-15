35efe46a2a511e27e1679a9f6c7fe3994bc43ae1
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-15 22:41:47 +0900

    코드업 1022

---
+++ src/Code_1022.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Code_1022 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String x = scanner.nextLine();
+		System.out.println(x);
+	}
+}
