5a224af507aa96593ca48527ab210d04967fcee2
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-15 22:00:54 +0900

    코드업 1018

---
+++ src/Code_1018.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code_1018 {
+	public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        String time=scanner.next();
+        
+        System.out.println(time);
+    }
+}
