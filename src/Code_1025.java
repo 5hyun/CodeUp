5d2fa2325b4f18bd0f61169c3193b3c938289992
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-16 20:31:42 +0900

    코드업 1025, 5자리가 아니고 n자리 수가 들어와도 돌아가게끔 풀려함

--- src/Code_1025.java
+++ src/Code_1025.java
@@ -19,3 +19,25 @@ public class Code_1025 {
 		System.out.println("[" + ary[0] + "]");
 	}
 }
+
+/*
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] cmd = sc.nextLine().split("");
+
+		System.out.println("["+Integer.parseInt(cmd[0]) * 10000+"]");
+		System.out.println("["+Integer.parseInt(cmd[1]) * 1000+"]");
+		System.out.println("["+Integer.parseInt(cmd[2]) * 100+"]");
+		System.out.println("["+Integer.parseInt(cmd[3]) * 10+"]");
+		System.out.println("["+Integer.parseInt(cmd[4]) * 1+"]");
+
+		sc.close();
+	}
+
+}
+*/
