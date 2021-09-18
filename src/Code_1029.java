dae629650366dce00a69dcc3401f726224a00b0b
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-19 00:29:36 +0900

    코드업 1029, double

---
+++ src/Code_1029.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code_1029 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double x = sc.nextDouble();
+		
+		System.out.printf("%.11f", x);
+	}
+}
