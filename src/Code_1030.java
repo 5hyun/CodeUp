35e5ff3e9c993c5a29d084654e68b40c4edc61f4
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-19 00:32:00 +0900

    코드업 1030

---
+++ src/Code_1030.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code_1030 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long x = sc.nextLong();
+		
+		System.out.println(x);
+	}
+}
