857cd9c7fa6c3945e5eb86e11dacfbc5a14fe927
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-19 23:05:18 +0900

    코드업 1033, %X로 하면 대문자로 출력

---
+++ src/Code_1033.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code_1033 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		System.out.printf("%X", x);
+	}
+}
