43c0f8a0ee05de2c7ed7cbd1ce6e595244ceea61
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-20 15:48:07 +0900

    코드업 1037

---
+++ src/Code_1037.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Code_1037 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		char a = (char)x;
+		System.out.println(a);
+	}
+}
