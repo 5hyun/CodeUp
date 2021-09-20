5de88e3ad04e31150fdf8f7f5d87eb6880084b9b
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-20 15:35:41 +0900

    코드업 1036, 문자를 아스키코드 int로 바꾸기 charAt() 함수 활용

---
+++ src/Code_1036.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Code_1036 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char x = sc.nextLine().charAt(0);
+		int a = (int)x;
+		
+		System.out.printf("%d", a);
+	}
+}
