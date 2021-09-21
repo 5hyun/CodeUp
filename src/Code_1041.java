69547ed8036765446528f35c0b6823886f5adc90
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-21 20:28:30 +0900

    코드업 1041, 입력 받은 문자 다음 문자 출력, charAt() 활용

---
+++ src/Code_1041.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Code_1041 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char x = sc.nextLine().charAt(0);
+		
+		System.out.println((char)(x+1));
+	}
+}
