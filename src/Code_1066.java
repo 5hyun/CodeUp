b309bd7989cde817c053888f2e98dd376805abd4
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-27 17:23:42 +0900

    코드업 1066

---
+++ src/Code_1066.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code_1066 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		System.out.println(a%2==0?"even":"odd");
+		System.out.println(b%2==0?"even":"odd");
+		System.out.println(c%2==0?"even":"odd");
+	}
+}
