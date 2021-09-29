ecfc187aa65ff11f165cd5ff0cd17990cca62f2b
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-29 18:05:29 +0900

    코드업 1071, 자바에는 goto문이 없다

---
+++ src/Code_1071.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code_1071 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			long x = sc.nextLong();
+			
+			if(x==0)
+				break;
+			System.out.println(x);
+		}
+	}
+}
