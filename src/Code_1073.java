2aedea9ecbacac7715118f4e1b5ae6ba543b17e7
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-30 18:49:48 +0900

    코드업 1073

---
+++ src/Code_1073.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code_1073 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int x = sc.nextInt();
+			
+			if(x==0)
+				break;
+			System.out.println(x);
+		}
+	}
+}
