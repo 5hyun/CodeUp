2f1f54b8a4064eac555e498527ccdeae09aa5611
5hyun <86971770+5hyun@users.noreply.github.com>
2021-09-30 19:08:29 +0900

    코드업 1076

---
+++ src/Code_1076.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Code_1076 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char x = sc.nextLine().charAt(0);
+		
+		for(char i = 'a'; i <= x; i++) {
+			System.out.printf("%c ", i);
+		}
+	}
+}
