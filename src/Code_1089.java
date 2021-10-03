68cd3871ab04a340eeae2259c1737b4d94e46a8f
5hyun <86971770+5hyun@users.noreply.github.com>
2021-10-03 20:33:03 +0900

    코드업 1089

---
+++ src/Code_1089.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Code_1089 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int d = sc.nextInt();
+		int n = sc.nextInt();
+
+		for(int i = 0; i<n-1; i++) {
+			a+=d;
+		}
+		System.out.println(a);
+	}
+}
